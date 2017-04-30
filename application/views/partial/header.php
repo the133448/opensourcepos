469f0c713cd563f4b327b265853ffca1c8491a02
jekkos <jeroen.peelaerts@gmail.com>
2017-04-30 14:48:42 +0200

    Update .gitignore

--- application/views/partial/header.php
+++ application/views/partial/header.php
@@ -74,7 +74,7 @@
 		<link rel="stylesheet" type="text/css" href="dist/style.css"/>
 		<!-- end mincss template tags -->
 		<!-- start minjs template tags -->
-		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=88dbff2d6f"></script>
+		<script type="text/javascript" src="dist/opensourcepos.min.js?rel=871dbc6755"></script>
 		<!-- end minjs template tags -->
 	<?php endif; ?>
 
